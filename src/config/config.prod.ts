d0e868b8cf7f0c3410c58d76a05ade4878936496
Nikola Mihaylov <ni4kata_94@mail.bg>
2023-10-25 11:44:31 +0300

    Add stage/prod configs

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -2,18 +2,15 @@ import { LogImplementation } from '@infrastructure/logger/LoggerFactory';
 import { Config } from './Config';
 import { LogLevel } from '@infrastructure/logger/Logger';
 import { parseBoolean, parseEnum, parseNumber, parseString } from './env-parsers';
-import { config as loadDotEnvConfig } from 'dotenv';
-
-loadDotEnvConfig();
 
 const config: Config = {
   log: {
     impl: parseEnum<LogImplementation>('LOG_IMPL', Object.values(LogImplementation), LogImplementation.Console),
-    level: parseEnum<LogLevel>('LOG_LEVEL', Object.values(LogLevel), LogLevel.Debug),
+    level: parseEnum<LogLevel>('LOG_LEVEL', Object.values(LogLevel), LogLevel.Info),
   },
   server: {
-    port: parseNumber('SERVER_PORT', 3000),
-    enableCORS: parseBoolean('SERVER_ENABLE_CORS', true),
+    port: parseNumber('SERVER_PORT', 80),
+    enableCORS: parseBoolean('SERVER_ENABLE_CORS', false),
   },
   db: {
     database: parseString('DB_NAME'),
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,16 @@
 import { Config } from './Config';
 export const isTesting = process.env.NODE_ENV === 'test';
+export const isStaging = process.env.NODE_ENV === 'staging';
+export const isProduction = process.env.NODE_ENV === 'production';
 
 export function loadConfig(): Config {
   let configPath: string;
   if (isTesting) {
     configPath = './config.test';
+  } else if (isStaging) {
+    configPath = './config.stage';
+  } else if (isProduction) {
+    configPath = './config.prod';
   } else {
     configPath = './config.dev';
   }
