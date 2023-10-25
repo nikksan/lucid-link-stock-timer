9c0b7bca0abd5d36b2e28bc5314467412fbe1b6a
Nikola Mihaylov <ni4kata_94@mail.bg>
2023-10-25 12:35:44 +0300

    Added readme

---
+++ README.md
@@ -0,0 +1,59 @@
+# Lucid Link Stock Timer
+
+## Summary
+
+Minimalistic service providing best entry and exit based on a stock price history.
+
+## Requirements
+
+* NodeJS v16.10.0 or newer
+* [PostgreSQL Server 15.4 or newer](https://www.postgresql.org/docs/current/tutorial-install.html).
+
+## Setup
+
+### Install required packages
+
+Execute `npm install`
+
+### Configure local environment variables
+
+- Execute `cp .env.sample .env` in the project root directory
+
+## Environment setup
+
+Current working environment could be changed by setting `NODE_ENV` environment variable. Available options are `development` (default), `testing`, `staging` and `production`.
+
+## Usage
+
+* `npm start` - starts the service
+* `npm run test` - runs the tests
+* `npm run migrate:up` - runs migrations
+* `npm run seed <from> <records>` - runs the seeder, expects two arguments - start date and number of records to insert
+
+For example.
+```
+$ npm run seed "2020-01-01 00:00:00" 1000000
+```
+
+## Suggested flow
+```
+$ cp .env.sample .env
+$ npm run migrate:up
+$ npm run seed "2023-01-01 00:00:00" 2000000
+$ npm run start
+```
+
+## Configuration
+
+Configuration options could be provided by either setting them as environment variables, when the server is run or by putting
+them in the `.env` file. Following options are supported:
+
+* `LOG_LEVEL` - log level at which messages should be logged. Following log levels are supported.
+  (in priority order, lowest to highest): `error`, `warn`, `info`, `debug`. Defaults to `debug` for dev and `info` for stage/prod
+* `SERVER_PORT` - self-explanatory. Defaults to `3000` for dev and `80` for stage/prod
+* `SERVER_ENABLE_CORS` - boolean, whether to allow cors or not. Defaults to `true` for dev and `false` for stage/prod
+* `DB_NAME` - self-explanatory
+* `DB_USER` - self-explanatory
+* `DB_PASSWORD` - self-explanatory
+* `DB_HOST` - self-explanatory, defaults to `5432`
+* `DB_PORT` - self-explanatory, defaults to `localhost`
--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -5,24 +5,24 @@ import { Client } from 'pg';
 import moment from 'moment';
 
 (async () => {
-  const argv = parseCliArgs(process.argv.slice(2));
-  if (!argv.from) {
-    throw new TypeError('Expected --from to be provided');
+  const [from, records] = parseCliArgs(process.argv.slice(2))._;
+
+  if (!from) {
+    throw new TypeError('Expected <from> to be provided');
   }
 
-  const startDate = new Date(argv.from);
-  if (startDate.toString() === 'Invalid Date') {
-    throw new TypeError('Expected --from to be valid date');
+  if (new Date(from).toString() === 'Invalid Date') {
+    throw new TypeError('Expected <from> to be valid date');
   }
 
-  if (!argv.records) {
-    throw new TypeError('Expected --records to be provided');
+  if (!records) {
+    throw new TypeError('Expected <records> to be provided');
   }
 
   const batchSize = 1000;
-  const recordsAsInt = parseInt(argv.records);
+  const recordsAsInt = parseInt(records);
   if (!recordsAsInt || recordsAsInt <= 0 || recordsAsInt % batchSize) {
-    throw new TypeError(`Expected --records to be positive and divisible by ${batchSize}`);
+    throw new TypeError(`Expected <records> to be positive and divisible by ${batchSize}`);
   }
 
   const config = loadConfig();
@@ -30,9 +30,15 @@ import moment from 'moment';
   await client.connect();
 
   let values: Array<[Date, number]> = [];
-
+  let lastDate: Date | null = null;
+  let firstDate: Date | null = null;
   for (let i = 0; i < recordsAsInt; i++) {
-    const dateToInsert = moment.utc(argv.from).add(i, 'seconds').set('milliseconds', 0).toDate();
+    const dateToInsert = moment.utc(from).add(i, 'seconds').set('milliseconds', 0).toDate();
+    if (firstDate === null) {
+      firstDate = dateToInsert;
+    }
+
+    lastDate = dateToInsert;
     values.push([dateToInsert, geRandomPriceBetween(10, 1000)]);
 
     if (i % batchSize === 0) {
@@ -45,7 +51,11 @@ import moment from 'moment';
   }
 
   await client.end();
-  console.log('Done with insertion.');
+  if (!firstDate || !lastDate) {
+    return;
+  }
+
+  console.log(`Done with insertion. Available range: [${firstDate.toISOString()}, ${lastDate.toISOString()}]`);
 })();
 
 function geRandomPriceBetween(min: number, max: number) {
